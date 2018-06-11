1eccce9e12725b91e7ede6c4f774d379fb3409c5
unknown <trieunx@hancts-02-PC.vietinbanksc.vn>
2018-06-11 15:36:53 +0700

    test

--- Worker/src/worker/Worker.java
+++ Worker/src/worker/Worker.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package worker;
-
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 /**
  *
  * @author trieunx
@@ -14,8 +18,32 @@ public class Worker {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        Document doc = Jsoup.connect("https://xosominhngoc.net.vn/ajax/?view=ketqua&id=mien-bac&date=04-06-2018").get();
+        Elements kqsx = doc.select("div.box_kqxs");
+        for (Element el : kqsx) {
+            String id = el.attr("id");
+
+            System.out.println("===============:" + id);
+            //boc ket qua tung ngay
+            Elements kq = el.select("table tr");
+            int index = 0;
+            for (Element tr : kq) {
+                //phat hanh
+                if (index == 0) {
+                    Element phathanh = tr.selectFirst("td.tentinh a");
+                    //System.out.println("Tỉnh:"+ phathanh.attr("href").substring(1));
+                }else
+                {
+                   // Element giai = tr.selectFirst("td[class=giai^]");
+                    //System.out.println(giai.toString());
+                
+                }
+                index++;
+                System.out.println(tr.toString());
+            }
+        }
     }
     
 }
